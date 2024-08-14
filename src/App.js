a5bc08e14760e463493168280a8dceca6aa020a3
Ira Kovtoniuk <irishkakov1787@gmail.com>
2024-08-14 22:57:54 +0200

    cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper">
+		<header>
+			<div className="headerWrapper">
+				<div className="headerLeft">
+					<img src="/img/logo.svg" alt="Logo"/>
+					<div className="headerIcon">
+						<h3>REACT SNEAKERS</h3>
+						<p>The best sneakers store</p>
+					</div>	
+				</div>	
+				<ul className="headerRight">
+					<li>
+						<img src="/img/Group.svg" alt="group"></img>
+						<span>1256 $</span>
+					</li>
+					<li>
+						<img src="/img/favorite.svg" alt="favorite"></img>
+						<span>Favorite</span>
+					</li>
+					<li>
+						<img src="/img/user.svg" alt="user"></img>
+						<span>Profile</span>
+					</li>
+				</ul>
+			</div>
+		</header>
+		<main>
+		<div className="content">
+			<h1>All Sneakers</h1>
+		</div>
+
+		<div className="snicers">
+			<div className="card">
+				<img src="/img/favicon.svg" alt="favicon"></img>
+				<img width={133} height={112} src="/img/snicers/1.jpeg" alt="snicers"></img>
+				<h5>Men's Nike Blazer Mid Suede Sneakers</h5>
+				<div className="card_info">
+					<div>
+						<p>Price:</p>
+						<b> 230 $</b>
+					</div>
+					<button>
+						<img src="/img/plas.svg" alt="plas"></img>
+					</button>
+				</div>
+			</div>
+			<div className="card">
+				<img src="/img/favicon.svg" alt="favicon"></img>
+				<img width={133} height={112} src="/img/snicers/1.jpeg" alt="snicers"></img>
+				<h5>Men's Nike Blazer Mid Suede Sneakers</h5>
+				<div className="card_info">
+					<div>
+						<p>Price:</p>
+						<b> 230 $</b>
+					</div>
+					<button>
+						<img src="/img/plas.svg" alt="plas"></img>
+					</button>
+				</div>
+			</div>
+		</div>
+		
+		
+			
+		</main>	
+	</div>
   );
 }
 
